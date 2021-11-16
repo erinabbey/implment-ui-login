3710687b58e634afcf16507bc77794fbcb3cd1bb
Nguyen Ngoc Huyen <45890409+Noragamiiii@users.noreply.github.com>
2021-11-16 22:26:17 +0700

    login ui first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="login-content">
+        <div className="login-container">
+          <div className="login">
+            <div className="">
+              <h1 className="header">Welcome back!</h1>
+              <p className="subtitle">Please login to continue</p>
+            </div>
+
+            <div className="login-form-container">
+              <div className="form-input">
+                <label htmlFor="email" className="label">
+                  Email address
+                </label>
+                <input id="email" type="text" className="input"></input>
+              </div>
+              <div className="form-input">
+                <label htmlFor="password" className="label">
+                  Password
+                </label>
+                <input id="password" type="password" className="input"></input>
+              </div>
+              <div className="login-btn">
+                <button className="button">Button Text</button>
+              </div>
+              <div className="new-scratch-text">new to Scratch</div>
+              <div className="button-text">Button Text</div>
+            </div>
+          </div>
+        </div>
+        <div className="left-panel left-image">
+          <div className="text-container text">
+            <p>scratch</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
